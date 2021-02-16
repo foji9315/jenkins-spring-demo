c11b0761959b45b50f06403e1cbb818c8566c0fd
ivanflores15 <73487119+ivanflores15@users.noreply.github.com>
2021-02-16 11:35:21 -0600

    Basic container

---
+++ src/main/java/com/jenkins/pipelinedemo/PipelineDemoApplication.java
@@ -0,0 +1,12 @@
+package com.jenkins.pipelinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineDemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/jenkins/pipelinedemo/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.jenkins.pipelinedemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/greeting")
+    public String gretting() {
+        return "Pipe-line example success";
+    }
+}
